aa0175b1400020c72993536895211313f2b23fcd
phoenix2113 <mayur.sonawala13@gmail.com>
2021-09-13 01:00:48 +0530

    update

--- output.py
+++ output.py
@@ -1,22 +1,24 @@
+from multiprocessing import process
 import pandas as pd
 import fetch_status_code
 import multiprocessing
-duration = 5  
+
+duration=int(input("Enter the duration:- ", ))
 # reading two csv files
 data1 = pd.read_csv('table1.csv')
 data2 = pd.read_csv('table2.csv')
   
 # using merge function by setting how='inner'
-output1 = pd.merge(data1, data2, 
+outputTable = pd.merge(data1, data2, 
                    on='Server Name', 
                    how='inner')
   
 # displaying result
-x = output1.to_numpy()
+mergeTable = outputTable.to_numpy()
 
-proc1 = multiprocessing.Process(target=fetch_status_code.server_response, args=(x[0],5 ))
-proc2 = multiprocessing.Process(target=fetch_status_code.server_response, args=(x[1],5 ))
-proc3 = multiprocessing.Process(target=fetch_status_code.server_response, args=(x[2],5 ))
+proc1 = multiprocessing.Process(target=fetch_status_code.server_response, args=(mergeTable[0],duration ))
+proc2 = multiprocessing.Process(target=fetch_status_code.server_response, args=(mergeTable[1],duration ))
+proc3 = multiprocessing.Process(target=fetch_status_code.server_response, args=(mergeTable[2],duration ))
 
 proc1.start()
 proc2.start()
