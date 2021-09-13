4cacbf7bbe6c1e5cab954c44b34d8c7c32dcfd0d
phoenix2113 <mayur.sonawala13@gmail.com>
2021-09-13 18:15:46 +0530

    test

--- output.py
+++ output.py
@@ -4,9 +4,12 @@ import fetch_status_code
 import multiprocessing
 
 duration=int(input("Enter the duration:- ", ))
+location_table1 = input("Enter the location of table 1:- ", )
+location_table2 = input("Enter the location of table 2:- ", )
+
 # reading two csv files
-data1 = pd.read_csv('table1.csv')
-data2 = pd.read_csv('table2.csv')
+data1 = pd.read_csv(location_table1)
+data2 = pd.read_csv(location_table2)
   
 # using merge function by setting how='inner'
 outputTable = pd.merge(data1, data2, 
